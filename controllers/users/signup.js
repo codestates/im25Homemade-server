85e688ca563253d7994e277e61f21f4998f72dfb
JayKim <JayKim@users.noreply.github.com>
2021-02-04 23:10:23 +0900

    [server] ADD 유저 회원가입 API 구현
    유저 정보를 받아 데이터베이스에 저장하는 API 로직

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,12 +1,9 @@
-const { Users } = require('../../models');
+const { user } = require('../../models');
 module.exports = {
   post: (req, res) => {
     // TODO : 회원가입 로직 및 유저 생성 로직 작성
-    const { email, password, username, mobile } = req.body;
 
-    if (!email || !password || !username || !mobile) {
-      return res.status(422).send('insufficient parameters supplied');
-    }
+    const { email, name, password, nickname, mobile } = req.body;
 
     user
       .findOrCreate({
@@ -14,20 +11,20 @@ module.exports = {
           email: email,
         },
         defaults: {
+          name: name,
           password: password,
-          username: username,
+          nickname: nickname,
           mobile: mobile,
         },
       })
       .then(async ([user, created]) => {
         if (!created) {
-          return res.status(409).send('email exists');
+          return res.status(409).send('email already exists');
         }
-        const data = await user.get({ plain: true });
-        //? get, plain?
-        // console.log("here is signUpController");
-        // console.log(data);
-        res.status(201).json(data);
+        res.status(201).send('Created Successfully');
+      })
+      .catch(err => {
+        res.status(500).send(err);
       });
   },
 };
