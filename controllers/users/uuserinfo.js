90207ddda0b44384bb08a79d461aa2f44f7d0932
JayKim <JayKim@users.noreply.github.com>
2021-02-07 17:10:04 +0900

    [server] ADD 유저정보 업데이트 요청 API 구현
    유저의 accesstoken 으로 인증받은 사용자인지 판단 후,
    유저로부터 받은 새로운 데이터로 업데이트 후 업데이트된 정보를
    유저에게 제공하는 로직의 API

--- controllers/users/uuserinfo.js
+++ controllers/users/uuserinfo.js
@@ -1,6 +1,42 @@
-const { Users } = require('../../models');
+const { user } = require('../../models');
+const { isAuthorized } = require('../tokenFunctions');
+
 module.exports = {
-  post: (req, res) => {
+  patch: async (req, res) => {
     //TODO: 유저정보 업데이트 로직 작성
+
+    const accessTokenData = isAuthorized(req);
+    if (!accessTokenData) {
+      res.status(404).send('invalid user');
+    } else if (accessTokenData) {
+      const { password, nickname, mobile, avatar } = req.body;
+
+      const isUpdated = await user.update(
+        {
+          nickname: nickname,
+          password: password,
+          mobile: mobile,
+          avatar_url: avatar,
+          updatedAt: new Date(),
+        },
+        {
+          where: { id: accessTokenData.id },
+          returning: true,
+          plain: true,
+        },
+      );
+      if (!isUpdated) {
+        throw 'Error while Updating';
+      }
+      const returnedUpdatedUserinfo = await user.findOne({
+        where: { id: accessTokenData.id },
+      });
+      delete returnedUpdatedUserinfo.dataValues.password;
+      res
+        .status(200)
+        .json({ data: { userInfo: returnedUpdatedUserinfo.dataValues } });
+    } else {
+      res.status(500).send('err');
+    }
   },
 };
