bc38a705b725944989e14466c5b309bf2ae40a9a
atomsong92 <yesparrot@gmail.com>
2021-02-07 10:50:06 +0900

    [server] ADD 모든 레시피, 검색결과 API 기능 추가(미완성)
    검색기능 아직 미구현, 재설정 예정

--- controllers/recipes/recipes.js
+++ controllers/recipes/recipes.js
@@ -1,6 +1,29 @@
-const { Users } = require('../../models');
+const { content, category } = require('../../models');
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
 module.exports = {
-  get: (req, res) => {
+  get: async (req, res) => {
     //TODO: 모든 레시피 요청 로직 작성
+    const allRecipe = await content.findAll({
+      where: {},
+      include: [{ model: category, attributes: ['name'] }],
+    });
+
+    const searchWord = req.params.searchWord;
+    const searchResults = await content.findAll({
+      where: {
+        title: {
+          [Op.like]: '%' + searchWord + '%',
+        },
+      },
+      order: [['rate', 'DESC']],
+    });
+
+    if (!allRecipe) {
+      res.send(400).send('Bad Request');
+    } else if (allRecipe) {
+      res.status(200).send(searchResults);
+    }
+    res.status(500).send(err);
   },
 };
