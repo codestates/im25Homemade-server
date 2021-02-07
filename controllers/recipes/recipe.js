1da3371acc48b55fbff174cfd8f93d713e3d86bb
atomsong92 <yesparrot@gmail.com>
2021-02-07 10:48:23 +0900

    [server] ADD 레시피 상세정보 GET 요청 API 기능 구현
    레시피 정보 GET API 개발

--- controllers/recipes/recipe.js
+++ controllers/recipes/recipe.js
@@ -1,6 +1,35 @@
-const { Users } = require('../../models');
+const { content, image, category } = require('../../models');
 module.exports = {
-  get: (req, res) => {
-    //TODO: 레시피 상세정보 요청 로직 작성
+  get: async (req, res) => {
+    // 레시피 목록에서 한개 클릭 시 보이는 로직
+
+    const recipe = await content.findOne({
+      where: { id: req.body.id },
+    });
+
+    const image = await image.findOne({
+      where: { contentId: req.body.id },
+      attributes: ['image_url', 'order'],
+    });
+
+    const category = await category.findOne({
+      where: { id: category.id },
+    });
+
+    if (!recipe) {
+      res.status(400).send('Bad Request');
+    } else if (recipe) {
+      res.status(200).send({
+        data: {
+          recipe: {
+            ...category.dataValues,
+            ...recipe.dataValues,
+            ...image.dataValues,
+          },
+        },
+      });
+    }
+
+    res.status(500).send('err');
   },
 };
