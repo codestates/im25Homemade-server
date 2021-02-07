34594022cb69d11ca77917afdd5ee84fe9dad446
atomsong92 <yesparrot@gmail.com>
2021-02-07 10:35:14 +0900

    [server] ADD 레시피 글 작성 POST API 기능 구현
    레시피 글 작성 API  개발

--- controllers/recipes/content.js
+++ controllers/recipes/content.js
@@ -1,6 +1,38 @@
-const { Users } = require('../../models');
+const { content, image } = require('../../models');
+const jwt = require('jsonwebtoken');
 module.exports = {
-  post: (req, res) => {
+  post: async (req, res) => {
     //TODO: 레시피 글작성 요청 로직 작성
+
+    if (req.headers.authorization) {
+      const tokencode = req.headers.authorization.split(' ')[1];
+      const token = await jwt.verify(tokencode, process.env.ACCESS_SECERET);
+
+      const newContent = await content.create({
+        title: req.body.title,
+        content: req.body.content,
+        rate: 0,
+        rateCount: 0,
+        views: 0,
+        thumbnail_url: 'multer',
+        userId: token.id,
+        categoryId: req.body.categoryId,
+      });
+
+      await image.create({
+        name: req.body.name,
+        image_url: req.body.image_url,
+        order: req.body.order,
+        contentId: newContent.dataValues.id,
+      });
+
+      res.status(201).send({
+        data: { userInfo: newContent.dataValues },
+        message: 'ok',
+      });
+    } else if (!req.headers.authorization) {
+      res.status(401).send('invalid token');
+    }
+    res.status(500).send(err);
   },
 };
