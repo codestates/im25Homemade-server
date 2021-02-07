2467bf9f3a4fa7f6e3341545008f2a6cb3085fbb
JayKim <JayKim@users.noreply.github.com>
2021-02-07 23:32:53 +0900

    ucontent.js

--- controllers/users/ucontent.js
+++ controllers/users/ucontent.js
@@ -1,19 +1,58 @@
 const { content } = require('../../models');
+const { image } = require('../../models');
+const { isAuthorized } = require('../tokenFunctions');
+
 module.exports = {
-  post: async (req, res) => {
+  patch: async (req, res) => {
     //TODO: 글내용 업데이트 로직 작성
     console.log('hello');
     const accessTokenData = isAuthorized(req);
     if (!accessTokenData) {
       res.status(401).send('invalid token');
     } else if (accessTokenData) {
-      const { id } = accessTokenData;
-      const contentInfo = await content.findAll({ where: { userId: id } });
-      if (!contentInfo) {
-        return res.status(204).send('cannot find recipe');
-      }
+      const { contentId, imageurl, title, contents, thumbnailurl } = req.body;
+
       //! contentInfo 를 배열로 담아서 유저에게 전달. 고민 필요.
-      res.status(200).json({ data: { recipes: content } });
+
+      const isUpdated = await content.update(
+        {
+          title: title,
+          content: contents,
+          thumbnail_url: thumbnailurl,
+          updatedAt: new Date(),
+        },
+        {
+          where: { id: contentId },
+        },
+      );
+      const isImageUpdated = await image.update(
+        {
+          image_url: imageurl,
+        },
+        {
+          where: {
+            contentId: contentId, //! returning, plain 필요없음 => 다른 업데이트 로직 변경할 것.
+          },
+        },
+      );
+
+      if (!isUpdated || !isImageUpdated) {
+        throw 'Error while Updating';
+      }
+      const returnedUpdatedContent = await content.findOne({
+        where: { id: contentId },
+      });
+      const returnedUpdatedImage = await image.findOne({
+        where: { contentId: contentId },
+      });
+
+      return res.status(200).json({
+        data: {
+          contentInfo: returnedUpdatedContent.dataValues,
+          imageInfo: returnedUpdatedImage,
+        },
+        message: 'ok',
+      });
     } else {
       res.status(500).send('err');
     }
