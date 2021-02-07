44d11402afb09ed39eca99af2d77e5c98d32af5d
atomsong92 <yesparrot@gmail.com>
2021-02-07 11:46:19 +0900

    [server] ADD 유저 회원탈퇴퇴 DELETE API 기능 구현
    회원탈퇴 API 개발

--- controllers/users/duser.js
+++ controllers/users/duser.js
@@ -1,6 +1,32 @@
-const { Users } = require('../../models');
+const { user, content, image, categorie } = require('../../models');
+const jwt = require('jsonwebtoken');
 module.exports = {
-  post: (req, res) => {
-    //TODO: 유저 탈퇴 요청
+  delete: async (req, res) => {
+    //TODO: 유저 회원탈퇴 로직
+    if (req.headers.authorization) {
+      const tokencode = req.headers.authorization.split(' ')[1];
+      const token = await jwt.verify(tokencode, process.env.ACCESS_SECERET);
+
+      await user.destroy({
+        where: { id: token.id },
+      });
+
+      const contentInfo = await content.destroy({
+        where: { id: req.body.id },
+      });
+
+      await image.destroy({
+        where: { id: contentInfo.dataValues.id },
+      });
+
+      await categorie.destroy({
+        where: { id: contentInfo.dataValues.categoryId },
+      });
+
+      res.status(200).send('delete content successfully');
+    } else if (!req.headers.authorization) {
+      res.status(401).send('invalid token');
+    }
+    res.send(err);
   },
 };
